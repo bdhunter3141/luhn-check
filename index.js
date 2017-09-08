43fd7ffd3c192a3b6e774672f6cd1d6710a28283
Bryan Hunter <bdhunter3141@gmail.com>
2017-09-08 20:00:27 +0000

    Now able to return an array of even numbers and pass NaN test

--- index.js
+++ index.js
@@ -1,8 +1,27 @@
 const check = function(number) {
-
-
-
+//From the right, moving left, double every other digit
+  if (isNaN(number)) {
+    return false
+  }
+  let evenNumArr = []
+  let stringNumArr = []
+  let numArr = number.toString().split("").reverse()
+  for (let num in numArr) {
+    if (num % 2 === 0) {
+      stringNumArr.push(numArr[num])
+    }
+  }
+  stringNumArr.forEach(function(element) {
+    var newNum = Number(element);
+    evenNumArr.push(newNum)
+  });
+  console.log(evenNumArr)
+  // If doubled number greater than 9, add the two numbers to itself
+//Sum all digits
+  //If modulo 10 = 0
+  //Then it is valid
+  //else, false
 }
-
+check(239057240184903)
 
 module.exports = check
--- test/luhn-check-test.js
+++ test/luhn-check-test.js
@@ -4,11 +4,15 @@ var check = require("../index.js")
 
 describe("Luhn Check", function() {
   it("Should return a 'true' value if number is valid", function() {
-    var number = 7992739871
+    var number = 79927398713
     assert.isTrue(check(number))
   })
   it("Should return a 'false' value if number is invalid", function() {
-    var number = 7992736471
-    assert.isTrue(check(number))
+    var number = 79927398711
+    assert.isFalse(check(number))
+  })
+  it("Should return a 'false' value if input is NaN", function() {
+    var string = "I'm NaN!"
+    assert.isFalse(check(string))
   })
 })
