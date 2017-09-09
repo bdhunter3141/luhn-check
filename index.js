c944a5d6c9e834085429205e860302cefc495075
Bryan Hunter <bdhunter3141@gmail.com>
2017-09-09 03:49:08 +0000

    finished product, passing all tests

--- index.js
+++ index.js
@@ -1,5 +1,4 @@
 const check = function(number) {
-//From the right, moving left, double every other digit
   if (isNaN(number)) {
     return false
   }
@@ -9,7 +8,7 @@ const check = function(number) {
   let stringNumArr = []
   let numArr = number.toString().split("").reverse()
   for (let num in numArr) {
-    if (num % 2 === 0) {
+    if (num % 2 !== 0) {
       stringNumArr.push(numArr[num])
     } else {
       oddStringArr.push(numArr[num])
@@ -30,21 +29,22 @@ const check = function(number) {
     if (doubleArr[i].toString().length > 1) {
       var splitString = doubleArr[i].toString().split("")
       doubleArr[i] = addNumbers(splitString[0], splitString[1])
-    } console.log(doubleArr)
+    }
   }
+  var finishedArr = doubleArr.concat(oddNumArr)
+  var finalSum = null
+  for (var i = 0; i < finishedArr.length; i++) {
+    finalSum += finishedArr[i]
+  }
+  if (finalSum % 10 === 0) {
+    return true
+  } else {
+    return false
+  }
+}
 
 function addNumbers(x, y) {
   return Number(x) + Number(y)
 }
-
-
-
-  // If doubled number greater than 9, add the two numbers to itself
-//Sum all digits
-  //If modulo 10 = 0
-  //Then it is valid
-  //else, false
-}
-check(239057240184903)
 
 module.exports = check
